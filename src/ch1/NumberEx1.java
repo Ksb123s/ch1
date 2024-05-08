3bf18a2083ecaf53ae005fd81a13bc0826cf918d
sunbeom <sunbum1234@naver.com>
2024-05-08 12:11:13 +0900

    for문

--- src/ch1/NumberEx1.java
+++ src/ch1/NumberEx1.java
@@ -2,4 +2,9 @@ package ch1;
 
 public class NumberEx1 {
 
+    public static void main(String[] args) {
+        for (int i = 0; i < 0; i++) {
+            System.out.println(i);
+        }
+    }
 }
