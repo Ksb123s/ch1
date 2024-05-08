e4abf781aa8b4bb307dede079728eab72038b2b4
sunbeom <sunbum1234@naver.com>
2024-05-08 12:42:35 +0900

    Account 메소드 추가

--- src/ch2/Account.java
+++ src/ch2/Account.java
@@ -4,4 +4,8 @@ public class Account {
     private String Owner;
     private int balance;
     private String accountNno;
+
+    public String getAccountNo() {
+        return accountNno;
+    }
 }
